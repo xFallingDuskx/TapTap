2176b735f93cc84c4ab01423913e0895ceb42134
Stephon Ricks <stephon.ricks@duke.edu>
2022-09-29 14:08:26 +0200

    gets user input and added timer, input timing issue still exist

--- taptap.js
+++ taptap.js
@@ -1,8 +1,59 @@
+let user;
+let countdown;
+
+function getUserInput() {
+    let inputs = prompt("Enter your name and desired time limit (greater than 0) separated by a common (i.e. User,15). Invalid or no input will result in the default of Nameless,30 being used :)", "User,15");
+
+    if (inputs === null) {
+        user = "Nameless";
+        countdown = 30;
+    } else {
+        inputs = inputs.split(",");
+        user = inputs[0] ?? "Nameless";
+        countdown = inputs[1] ?? 30;
+    }
+    
+    const correct = confirm("Welcome " + user + "! You have decided to tap like crazy for " + countdown + " seconds! If this is incorrect, press the cancel button to re-enter the information. If this is correct, get ready to tap tap away :) The timer will start after the first click on the 'Tap' button. ")
+
+    if (!correct) {
+        getUserInput();
+    }
+}
+
+let countdownEl = document.getElementById("countdown-el");
+window.addEventListener("load", (event) => {
+    getUserInput();
+    countdownEl.innerText = countdown;
+});
+
+// window.onload = function () {
+//     getUserInput();
+//     countdownEl.innerText = countdown;
+// };
+
+let interval;
+let limit = countdown;
+function countdownInterval() {
+    if (countdown == 0) {
+        clearInterval(interval);
+        return;
+    }
+    countdown--;
+    countdownEl.innerText = countdown;
+}
+
+function startCountdown() {
+    interval = setInterval(countdownInterval, 1000);
+}
 
 let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
+    if (count == 0) {
+        startCountdown();
+    }
+
     count += 1;
     countEl.innerText = count;
 }
@@ -12,10 +63,8 @@ function roundToTwo(num) {
 }
 
 let scoreboardEl = document.getElementById("scoreboard-el");
-let user = "Username";
-
 let avg;
 function record() {
-    avg = roundToTwo(count / 30);
+    avg = roundToTwo(count / limit);
     scoreboardEl.innerHTML += ("<tr><td>" + user + "</td><td>" + count + "</td><td>" + avg + "</td></tr>");
 }
